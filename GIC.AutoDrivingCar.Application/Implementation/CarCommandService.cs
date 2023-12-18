94f58e41d6638f2630213713a1f9972b402aa948
manojkke <manojkke@gmail.com>
2023-12-18 14:05:54 +0800

    Added Application layer and updated gitignore

---
+++ GIC.AutoDrivingCar.Application/Implementation/CarCommandService.cs
@@ -0,0 +1,40 @@
+﻿using GIC.AutoDrivingCar.Application.Interface;
+using GIC.AutoDrivingCar.Domain.ValueObjects;
+using GIC.AutoDrivingCar.Infrastructure.Models;
+
+namespace GIC.AutoDrivingCar.Application.Implementation
+{
+    public class CarCommandService : ICarCommandService
+    {
+        private readonly Field _field;
+
+        public CarCommandService(Field field)
+        {
+            _field = field;
+        }
+
+        public void ExecuteCommands(Car car, string commands)
+        {
+            foreach (char command in commands)
+            {
+                switch (command)
+                {
+                    case 'F':
+                        if (_field.IsValidPosition(car))
+                        {
+                            car.MoveForward();
+                        }
+                        break;
+                    case 'L':
+                        car.TurnLeft();
+                        break;
+                    case 'R':
+                        car.TurnRight();
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Unknown command: {command}");
+                }
+            }
+        }
+    }
+}
---
+++ GIC.AutoDrivingCar.Application/Interface/ICarCommandService.cs
@@ -0,0 +1,9 @@
+﻿using GIC.AutoDrivingCar.Domain.ValueObjects;
+
+namespace GIC.AutoDrivingCar.Application.Interface
+{
+    interface ICarCommandService
+    {
+        void ExecuteCommands(Car car, string commands);
+    }
+}
